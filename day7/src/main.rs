a70bd0a7557873c7a87462fe32fdcb4b3dfc4a0f
Vasile Draguța <dragutavasile@gmail.com>
2025-12-07 21:38:35 +0200

    day7 part2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -21,12 +21,12 @@ fn solve1(
     current_column: usize,
     result: &mut i32,
 ) {
-    while current_row < diagram.len() - 1 && diagram[current_row][current_column] == '.' {
+    while current_row <= (diagram.len() - 1) && diagram[current_row][current_column] == '.' {
         diagram[current_row][current_column] = '|';
         current_row += 1;
     }
 
-    if current_row != diagram.len() - 1 {
+    if current_row < diagram.len() - 1 {
         if diagram[current_row][current_column] == '^' {
             *result += 1;
             solve1(diagram, current_row, current_column - 1, result);
@@ -35,10 +35,35 @@ fn solve1(
     }
 }
 
+fn solve2(diagram: &mut Vec<Vec<char>>, dp: &mut Vec<Vec<u64>>) {
+    for i in (0..diagram.len()).rev() {
+        for j in 0..diagram[i].len() {
+            if i == diagram.len() - 1 {
+                if diagram[i][j] == '|' {
+                    dp[i][j] = 1;
+                }
+            } else {
+                if diagram[i + 1][j] == '|' {
+                    dp[i][j] = dp[i + 1][j];
+                } else if diagram[i + 1][j] == '^' {
+                    dp[i][j] = dp[i + 1][j - 1] + dp[i + 1][j + 1];
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     let input_file = "src/input.txt";
     let (mut diagram, start_index) = read_input(input_file);
     let mut result = 0;
+    let mut dp: Vec<Vec<u64>> = Vec::new();
+    for _ in 0..diagram.len() {
+        dp.push(vec![0; diagram[0].len()]);
+    }
     solve1(&mut diagram, 1, start_index, &mut result);
+    solve2(&mut diagram, &mut dp);
+
     println!("{:?}", result);
+    println!("{:?}", dp[0][dp[0].len() / 2]);
 }
